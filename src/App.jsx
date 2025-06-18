2c46ea103de908d160329d4499e24950c8d6a545
medinalander66 <medinalander66@gmail.com>
2025-06-18 18:10:17 +0800

    Final Commit CSS Dark MODES

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import ToDoContainer from './components/Container/ToDoContainer'
 import Header from './components/Header'
 import './css/App.css'
+import Button from './components/Button'
 
 function App() {
   const [count, setCount] = useState(0)
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,32 @@
+import { useEffect, useState } from "react"
 import "../css/Header.css"
+import "../css/App.css"
+import Button from "./Button"
 export default function Header(){
+
+
+    const [darkMode, setDarkMode] = useState(false);
+    const toggleTheme = () =>{
+        setDarkMode( prev => !prev)
+    }
+
+    useEffect( () => {
+        document.body.className = darkMode ? 'dark' : 'light';
+        const toggleTheme=document.querySelector("toggle-theme")
+        
+    }, [darkMode])
+
+
+
     return(
-        <h1 className="bg-title">TO DO APP</h1>
+        <>
+            <header >
+            <div className='header'>
+                <h1>TO DO APP</h1>
+                <Button className="toggle-theme" action={toggleTheme} text={darkMode ? "Light" : "Dark"} />
+            </div>
+        </header>
+        </>
+
     )
 }

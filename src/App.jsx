d62cdb762735aa35f58c4244bd259b41d94942c2
medinalander66 <medinalander66@gmail.com>
2025-06-18 15:28:47 +0800

    Second Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import ToDoContainer from './components/Container/ToDoContainer'
 import Header from './components/Header'
-import './css/app.css'
-import Register from './components/Container/RegisterModal'
 
 function App() {
   const [count, setCount] = useState(0)
